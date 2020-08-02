083343558c5633efed61542f4d9552b8fc45b078
tokusumi <tksmtoms@gmail.com>
2020-08-02 12:02:18 +0900

    add github actions for build, pytest and codecov

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
 
-# pytest --cov=fastapi_cloudauth --cov=tests --cov-report=term-missing tests ${@}
-pytest --cov=./fastapi_cloudauth --cov-report=xml --disable-warnings
+pytest --cov=fastapi_cloudauth --cov=tests --cov-report=xml --disable-warnings tests/
