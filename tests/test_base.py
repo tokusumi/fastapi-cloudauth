cf391179158c84bfd8f8ff109eae6ef3b8928b09
tokusumi <tksmtoms@gmail.com>
2021-02-12 02:12:55 +0900

    add unittest pytest mark

--- tests/test_base.py
+++ tests/test_base.py
@@ -5,6 +5,7 @@ from fastapi.security import HTTPAuthorizationCredentials
 from fastapi_cloudauth.base import JWKS, TokenUserInfoGetter, TokenVerifier
 
 
+@pytest.mark.unittest
 def test_raise_error_invalid_set_scope():
     # scope_key is not declaired
     token_verifier = TokenVerifier(jwks=JWKS(keys=[]))
@@ -13,6 +14,7 @@ def test_raise_error_invalid_set_scope():
         token_verifier.scope("read:test")
 
 
+@pytest.mark.unittest
 def test_return_instance_with_scope():
     # scope method return new instance to give it for Depends.
     verifier = TokenVerifier(jwks=JWKS(keys=[]))
@@ -29,6 +31,7 @@ def test_return_instance_with_scope():
     assert obj.auto_error == verifier.auto_error, "return cloned objects"
 
 
+@pytest.mark.unittest
 @pytest.mark.parametrize(
     "scopes",
     [
@@ -59,6 +62,7 @@ def test_validation_scope(mocker, scopes):
     assert not obj.verify_scope(HTTPAuthorizationCredentials(scheme="", credentials=""))
 
 
+@pytest.mark.unittest
 def test_forget_def_user_info():
     with pytest.raises(AttributeError):
         TokenUserInfoGetter()
