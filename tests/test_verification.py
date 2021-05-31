77fff9c8d155e7c77e7841848d4becb329d70498
tokusumi <tksmtoms@gmail.com>
2021-06-01 00:42:16 +0900

    add test

--- tests/test_verification.py
+++ tests/test_verification.py
@@ -1,8 +1,15 @@
+from datetime import datetime, timedelta
+
 import pytest
 from fastapi import HTTPException
 from fastapi.security import HTTPAuthorizationCredentials
+from jose import jwt
+from starlette.status import HTTP_403_FORBIDDEN
 
+from fastapi_cloudauth import messages
 from fastapi_cloudauth.verification import JWKS, JWKsVerifier, ScopedJWKsVerifier
+
+from .helpers import _assert_verifier
 
 
 @pytest.mark.unittest
@@ -30,3 +37,46 @@ def test_malformed_token_handling():
     verifier = ScopedJWKsVerifier(jwks=JWKS(keys=[]), auto_error=False)
     assert not verifier._verify_scope(http_auth_with_malformed_token)
     assert not verifier.verify_token(http_auth_with_malformed_token)
+
+
+@pytest.mark.unittest
+def test_verify_token():
+    verifier = JWKsVerifier(jwks=JWKS(keys=[]))
+
+    # correct
+    token = jwt.encode(
+        {
+            "sub": "dummy-ID",
+            "exp": datetime.utcnow() + timedelta(hours=10),
+            "iat": datetime.utcnow(),
+        },
+        "dummy_secret",
+        headers={"alg": "HS256", "typ": "JWT", "kid": "dummy-kid"},
+    )
+    verifier._verify_claims(HTTPAuthorizationCredentials(scheme="a", credentials=token))
+
+    # token expired
+    token = jwt.encode(
+        {
+            "sub": "dummy-ID",
+            "exp": datetime.utcnow() - timedelta(hours=10),  # 10h before
+            "iat": datetime.utcnow(),
+        },
+        "dummy_secret",
+        headers={"alg": "HS256", "typ": "JWT", "kid": "dummy-kid"},
+    )
+    e = _assert_verifier(token, verifier)
+    assert e.status_code == HTTP_403_FORBIDDEN and e.detail == messages.NOT_VERIFIED
+
+    # token created at future
+    token = jwt.encode(
+        {
+            "sub": "dummy-ID",
+            "exp": datetime.utcnow() + timedelta(hours=10),
+            "iat": datetime.utcnow() + timedelta(hours=10),
+        },
+        "dummy_secret",
+        headers={"alg": "HS256", "typ": "JWT", "kid": "dummy-kid"},
+    )
+    e = _assert_verifier(token, verifier)
+    assert e.status_code == HTTP_403_FORBIDDEN and e.detail == messages.NOT_VERIFIED
